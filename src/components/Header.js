608b137dd1e92fa8636ba1e30fd8ae6938bd29ae
Prabhjot Singh <iprabhsingh0@gmail.com>
2021-05-13 15:31:31 -0400

    changed api url added some headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,13 @@ export default class Header extends Component {
 
      searchSongs= async ()=>{
          if(this.state.searchinput.length>2){
-    const res = await fetch(`https://api.deezer.com/search?q=${this.state.searchinput}`)
+    const res = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${this.state.searchinput}`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "ea6e9b3482msh61cbf08243f1742p1969d4jsn9af4f1dec164",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
+    });
      const data = await res.json();
      console.log(data);
      this.setState({
@@ -46,7 +52,7 @@ export default class Header extends Component {
            <div id="logo"> MusicPlay <img alt="" src="https://img.icons8.com/color/96/000000/audio-wave2.png"/></div>
             <div id="searchbox">
                 <input id="searchinput" value ={this.state.searchinput}
-                onChange={(e)=>this.searchInput(e)} onKeyPress={(e)=>this.onKeyPress(e) } placeholder="search songs ..."/>
+                onChange={(e)=>this.searchInput(e)} onKeyPress={(e)=>this.onKeyPress(e) } placeholder="Search..."/>
                 <button type="submit" onClick={()=>this.searchSongs()} id="submitbutton">Search</button>
             </div>
           </header>
